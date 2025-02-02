35510dc092c16001b7ead80e76ac5e7b1046e99b
Dominika <ddd.wilk@gmail.com>
2025-02-02 12:31:52 +0100

    Added delete confirmation dialog and collapsing inactive scenes

--- storyboard_tool.py
+++ storyboard_tool.py
@@ -7,7 +7,7 @@ from krita import (
     Krita,
     InfoObject,
     Selection )
-from PyQt5.QtWidgets import QDockWidget, QTextEdit
+from PyQt5.QtWidgets import QDockWidget, QTextEdit, QMessageBox, QShortcut
 from PyQt5.QtWidgets import (
     QPushButton,
     QStatusBar,
@@ -41,9 +41,9 @@ def isScene(node):
 
 
 def getScene(node):
-    if isRoot(node):
+    if node is None or isRoot(node):
         return None
-    if isScene(node):
+    elif isScene(node):
         return node
     else:
         return getScene(node.parentNode())
@@ -57,29 +57,61 @@ def createBackgroundLayer():
 def createEmptyLayer():
     return KI.activeDocument().createNode("Paint Layer", "paintlayer")
 
+# UI Helpers
+
+def deleteConfirmationDialog():
+    msg = QMessageBox()
+    msg.setIcon(QMessageBox.Question)
+    msg.setText("Are you sure you want to delete the scene?")
+    msg.setWindowTitle("Delete scene")
+    msg.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+    
+    if response := msg.exec_():
+        return response == QMessageBox.Yes
+    
+
+# Collapses all scenes except active one
+def setActiveScene(activeScene):
+    KI.activeDocument().setActiveNode(activeScene)
+    allScenes = getAllScenes()
+    for scene in allScenes:
+        scene.setCollapsed(scene.uniqueId() != activeScene.uniqueId())
+
+
+# UI Actions
+
 def createScene():
     root = KI.activeDocument().rootNode()
     scene = KI.activeDocument().createNode("SNG ", "grouplayer")
     scene.addChildNode(createBackgroundLayer(), None)
     scene.addChildNode(createEmptyLayer(), None)
     root.addChildNode(scene, getScene(KI.activeDocument().activeNode()))
+    setActiveScene(scene)
+
 
 def nextScene():
     allScenes = getAllScenes()
     currentScene = getScene(KI.activeDocument().activeNode())
     for i in range(len(allScenes) - 1):
         if allScenes[i].uniqueId() == currentScene.uniqueId():
-            currentScene.setCollapsed(True)
-            KI.activeDocument().setActiveNode(allScenes[i + 1])
+            setActiveScene(allScenes[i + 1])
 
 
 def prevScene():
     allScenes = getAllScenes()
     currentScene = getScene(KI.activeDocument().activeNode())
     for i in range(1, len(allScenes)):
-        if allScenes[i].uniqueId() == currentScene.uniqueId():
-            currentScene.setCollapsed(True)
-            KI.activeDocument().setActiveNode(allScenes[i - 1])
+        if allScenes[i].uniqueId() == currentScene.uniqueId():           
+            setActiveScene(allScenes[i - 1])
+
+
+def deleteScene():
+    if deleteConfirmationDialog():
+        currentScene = getScene(KI.activeDocument().activeNode())
+        nextScene() # Will select next scene
+        if currentScene is not None:
+            currentScene.remove()
+
 
 class StoryboardToolWidget(DockWidget):
     def __init__(self):
@@ -102,6 +134,11 @@ class StoryboardToolWidget(DockWidget):
         prevSceneButton.setToolTip("Go to previous scene")
         hboxlayout.addWidget(prevSceneButton)
         prevSceneButton.released.connect(partial(prevScene))
+
+        deleteSceneButton = QPushButton("Delete Scene")
+        deleteSceneButton.setToolTip("Delete currently selected scene(s)")
+        hboxlayout.addWidget(deleteSceneButton)
+        deleteSceneButton.released.connect(partial(deleteScene))
 
         uiContainer.setLayout(hboxlayout)
         self.setWidget(uiContainer)
